7e29ec00ce3a41bf7210f78ea4b05da76cdd0297
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-03-26 16:08:45 -0300

    feat: create swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import 'reflect-metadata'
 import express from 'express'
 
+import swaggerUI from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
+
 import './database'
+
+import './shared/container'
+
+import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Server start on port, 3000')
